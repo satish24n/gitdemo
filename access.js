625e28b7d973a5f0019ee11da1316cdc01bb7c81
satish <android24n@gmail.com>
2020-04-16 03:03:31 +0530

    asian guy atchitech day 1

--- access.js
+++ access.js
@@ -17,3 +17,5 @@ var obj = {
 };
 console.log(obj.color);
 console.log(obj.engine);
+
+added at 2:36AM 16April
---
+++ sampleTest.ts
@@ -0,0 +1,3 @@
+this is something completely new..this was empty file and I am adding 
+new lines to it.
+I hope i do well in this company.
